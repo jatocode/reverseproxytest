e97acb5888165aff87e00d5ed59556d096d49c59
Tobias <tobias.jansson@gmail.com>
2024-02-01 21:18:27 +0100

    baklänkar och felaktiga länkar

--- service2/script.js
+++ service2/script.js
@@ -6,3 +6,12 @@ fetch('assets/data.txt')
     .catch(error => {
         console.error('Error:', error);
     });
+
+fetch('/assets/data.txt')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('data2').textContent = data;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
